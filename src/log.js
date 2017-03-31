0e68d56175feda5191f08fe4948cb463b611eea6
Knut Ahlers <knut@ahlers.me>
2017-04-01 01:24:54 +0200

    Implement log levels
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/log.js
+++ src/log.js
@@ -1,8 +1,40 @@
+let LOGLEVEL_DEBUG = 0;
+let LOGLEVEL_INFO = 1;
+let LOGLEVEL_WARN = 2;
+let LOGLEVEL_ERROR = 3;
+
+let sendLog = function(message, level) {
+  if (Memory.log_level > level) return;
+  console.log(JSON.stringify(message));
+}
+
 module.exports = {
 
   debug: function(obj) {
-    obj._level = 'DBG';
-    console.log(JSON.stringify(obj))
+    obj._level = 'debug';
+    sendLog(obj, LOGLEVEL_DEBUG);
   },
+
+  info: function(obj) {
+    obj._level = 'info';
+    sendLog(obj, LOGLEVEL_INFO);
+  },
+
+  warn: function(obj) {
+    obj._level = 'warn';
+    sendLog(obj, LOGLEVEL_WARN);
+  },
+
+  error: function(obj) {
+    obj._level = 'error';
+    sendLog(obj, LOGLEVEL_ERROR);
+  },
+
+  level: {
+    debug: LOGLEVEL_DEBUG,
+    info: LOGLEVEL_INFO,
+    warn: LOGLEVEL_WARN,
+    error: LOGLEVEL_ERROR,
+  }
 
 }
--- src/task.migrate.js
+++ src/task.migrate.js
@@ -1,8 +1,15 @@
+let log = require('log');
+
 module.exports = function(creep) {
 
   // V1: All role-less creeps were initially harvesters
   if (creep.memory.role == undefined) {
     creep.memory.role = 'harvester';
+  }
+
+  // V2: Set a default log level in global memory
+  if (Memory.log_level == undefined) {
+    Memory.log_level = log.level.info;
   }
 
 };
