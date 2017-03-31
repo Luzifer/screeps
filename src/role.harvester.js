179f25a6005ee730440029d575bbaeebeb70f793
Knut Ahlers <knut@ahlers.me>
2017-03-31 20:30:17 +0200

    Load energy into extensions
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -6,10 +6,21 @@ module.exports = function(spawn, creep) {
       creep.moveTo(source);
     }
   } else {
+    let extensions = spawn.room.find(FIND_MY_STRUCTURES, {
+      filter: function(o) {
+        return o.structureType == STRUCTURE_EXTENSION && o.energy < o.energyCapacity
+      }
+    });
+
     if (spawn.energy < spawn.energyCapacity) {
       creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? creep.memory.mode : 'harvest');
       if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(spawn);
+      }
+    } else if (extensions.length > 0) {
+      creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? creep.memory.mode : 'harvest');
+      if (creep.transfer(extensions[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(extensions[0]);
       }
     } else {
       creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? 'upgrade' : 'harvest');
