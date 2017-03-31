dd458b9733dc70de17a4ec4ddd7319541271dc47
Knut Ahlers <knut@ahlers.me>
2017-03-31 18:08:27 +0200

    Fix hanging harvesters
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -7,6 +7,7 @@ module.exports = function(spawn, creep) {
     }
   } else {
     if (spawn.energy < spawn.energyCapacity) {
+      creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? creep.memory.mode : 'harvest');
       if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(spawn);
       }
