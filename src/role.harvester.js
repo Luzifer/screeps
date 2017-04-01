dc6d912f118d08fd8dd3cba1d27439ee32789b0a
Knut Ahlers <knut@ahlers.me>
2017-04-01 19:30:54 +0200

    Rewrite harvester logic
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,43 +1,130 @@
+let log = require('log');
+
 module.exports = function(spawn, creep) {
-  if (creep.carry[RESOURCE_ENERGY] < creep.carryCapacity && creep.memory.mode == 'harvest') {
-    let source = creep.pos.findClosestByRange(FIND_SOURCES);
+  log.debug({
+    _fkt: 'harvest',
+    creep: creep,
+    memory: creep.memory,
+  })
 
-    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(source);
-    }
-  } else {
-    let extensions = spawn.room.find(FIND_MY_STRUCTURES, {
-      filter: function(o) {
-        return o.structureType == STRUCTURE_EXTENSION && o.energy < o.energyCapacity
+  if (creep.memory.target == undefined) {
+    if (creep.memory.mode == 'harvest') {
+      // In case we're new to harvest mode lets choose a target
+      let droppedEnergyPile = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+      if (droppedEnergyPile) {
+        creep.memory.target = {
+          id: droppedEnergyPile.id,
+          type: 'droppedEnergyPile'
+        }
+      } else {
+        let source = creep.pos.findClosestByRange(FIND_SOURCES);
+        creep.memory.target = {
+          id: source.id,
+          type: 'source'
+        }
       }
-    });
 
-    let towers = spawn.room.find(FIND_MY_STRUCTURES, {
-      filter: function(o) {
-        return o.structureType == STRUCTURE_TOWER && o.energy < o.energyCapacity
-      }
-    });
-
-    if (spawn.energy < spawn.energyCapacity) {
-      creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? 'fill' : 'harvest');
-      if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(spawn);
-      }
-    } else if (towers.length > 0) {
-      creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? 'fill' : 'harvest');
-      if (creep.transfer(towers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(towers[0]);
-      }
-    } else if (extensions.length > 0) {
-      creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? 'fill' : 'harvest');
-      if (creep.transfer(extensions[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(extensions[0]);
-      }
     } else {
-      creep.memory.mode = (creep.carry[RESOURCE_ENERGY] > 0 ? 'upgrade' : 'harvest');
-      if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.room.controller);
+      let extensions = spawn.room.find(FIND_MY_STRUCTURES, {
+        filter: function(o) {
+          return o.structureType == STRUCTURE_EXTENSION && o.energy < o.energyCapacity
+        }
+      });
+
+      let towers = spawn.room.find(FIND_MY_STRUCTURES, {
+        filter: function(o) {
+          return o.structureType == STRUCTURE_TOWER && o.energy < o.energyCapacity
+        }
+      });
+
+      if (spawn.energy < spawn.energyCapacity) {
+        creep.memory.target = {
+          id: spawn.id,
+          type: 'spawn'
+        }
+      } else if (extensions.length > 0) {
+        creep.memory.target = {
+          id: extensions[0].id,
+          type: 'extension'
+        }
+      } else if (towers.length > 0) {
+        creep.memory.target = {
+          id: towers[0].id,
+          type: 'tower'
+        }
+      } else {
+        creep.memory.target = {
+          id: creep.room.controller.id,
+          type: 'controller'
+        }
       }
     }
+  }
+
+  let target = Game.getObjectById(creep.memory.target.id);
+  if (target == null) {
+    log.error({
+      _fkt: 'harvest',
+      error: 'Was assigned unkown target',
+      target: creep.memory.target,
+    })
+    creep.memory.target = undefined;
+    return;
+  }
+
+  // Decide what action to take on our target
+  let action = creep.transfer;
+  let args = [target, RESOURCE_ENERGY];
+  let _action = 'transfer';
+  if (creep.memory.target.type == 'controller') {
+    action = creep.upgradeController;
+    args = [target];
+    _action = 'upgrade';
+  } else if (creep.memory.target.type == 'droppedEnergyPile') {
+    action = creep.pickup;
+    args = [target];
+    _action = 'pickup';
+  } else if (creep.memory.target.type == 'source') {
+    // From those in this list only a source should regenerate
+    action = creep.harvest;
+    args = [target];
+    _action = 'harvest';
+  }
+
+  log.debug({
+    _fkt: 'harvest',
+    creep: creep,
+    action: _action,
+    target: creep.memory.target,
+  })
+
+  let result = action.apply(creep, args);
+  switch (result) {
+    case ERR_NOT_IN_RANGE:
+      creep.moveTo(target);
+      break;
+    case ERR_NOT_FOUND:
+    case ERR_INVALID_TARGET:
+      creep.memory.target = undefined
+      break;
+    case ERR_NOT_ENOUGH_ENERGY:
+    case ERR_NOT_ENOUGH_RESOURCES:
+      creep.memory.mode = 'harvest';
+      creep.memory.target = undefined;
+      break;
+    case ERR_FULL:
+      creep.memory.mode = 'move';
+      creep.memory.target = undefined;
+      break;
+
+    case OK:
+      break;
+
+    default:
+      log.error({
+        _fkt: 'harvest',
+        error: 'Got unknown error code',
+        code: result,
+      })
   }
 };
