c24b679b00f1ef8dc466b8aeccea92a2b13d1493
Knut Ahlers <knut@ahlers.me>
2017-04-13 22:25:12 +0200

    Priorize harvester building
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/config.js
+++ src/config.js
@@ -1,28 +1,28 @@
 module.exports = {
 
-  creepRequirements: {
-    builder: {
-      body: [
-        [WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], // Bigger model (8 extensions) - 700E
-        [WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], // Advanced model (4 extensions) - 500E
-        [WORK, MOVE, MOVE, CARRY, CARRY], // Basic model - 300E
-      ],
-      count: 2,
+    creepRequirements: {
+        harvester: {
+            body: [
+                [WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], // Bigger model (8 extensions) - 700E
+                [WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], // Advanced model (4 extensions) - 500E
+                [WORK, MOVE, MOVE, CARRY, CARRY], // Basic model - 300E
+            ],
+            count: 4,
+        },
+        builder: {
+            body: [
+                [WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], // Bigger model (8 extensions) - 700E
+                [WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], // Advanced model (4 extensions) - 500E
+                [WORK, MOVE, MOVE, CARRY, CARRY], // Basic model - 300E
+            ],
+            count: 2,
+        },
+        fighter: {
+            body: [
+                [], // Basic model
+            ],
+            count: 0,
+        },
     },
-    harvester: {
-      body: [
-        [WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY], // Bigger model (8 extensions) - 700E
-        [WORK, WORK, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY], // Advanced model (4 extensions) - 500E
-        [WORK, MOVE, MOVE, CARRY, CARRY], // Basic model - 300E
-      ],
-      count: 4,
-    },
-    fighter: {
-      body: [
-        [], // Basic model
-      ],
-      count: 0,
-    },
-  },
 
 };
