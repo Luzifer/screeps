4efcdf1773d26ac446cb2ab453b1ba3206d545fe
Knut Ahlers <knut@ahlers.me>
2017-04-15 09:04:27 +0200

    Add more tower positions
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/task.construct.js
+++ src/task.construct.js
@@ -1,59 +1,62 @@
 let log = require('log');
 
 let ensureBuilding = function(room, pos, structureType) {
-  let found = room.lookAt(pos);
-  for (let i = 0; i < found.length; i++) {
-    if (found[i].type == LOOK_STRUCTURES || found[i].type == LOOK_CONSTRUCTION_SITES || (found[i].type == 'terrain' && found[i].terrain != 'plain')) {
-      return;
+    let found = room.lookAt(pos);
+    for (let i = 0; i < found.length; i++) {
+        if (found[i].type == LOOK_STRUCTURES || found[i].type == LOOK_CONSTRUCTION_SITES || (found[i].type == 'terrain' && found[i].terrain != 'plain')) {
+            return;
+        }
     }
-  }
 
-  log.debug({
-    _fkt: 'ensureBuilding',
-    construct: room.createConstructionSite(pos, structureType),
-  });
+    log.debug({
+        _fkt: 'ensureBuilding',
+        construct: room.createConstructionSite(pos, structureType),
+    });
 }
 
 let constructExtensions = function(room) {
-  // Construct extensions around the spawn
-  if (room.controller.level > 1) {
-    room.find(FIND_MY_STRUCTURES, {
-      filter: {
-        structureType: STRUCTURE_SPAWN
-      }
-    }).forEach(function(spawn, idx) {
-      let relativeSpots = [];
+    // Construct extensions around the spawn
+    if (room.controller.level > 1) {
+        room.find(FIND_MY_STRUCTURES, {
+            filter: {
+                structureType: STRUCTURE_SPAWN
+            }
+        }).forEach(function(spawn, idx) {
+            let relativeSpots = [];
 
-      for (let i = 0; i < 6; i += 2) {
-        for (let j = 0; j < 6; j += 2) {
-          relativeSpots.push([i, j], [i * -1, j], [i * -1, j * -1], [i, j * -1])
-        }
-      }
+            for (let i = 0; i < 6; i += 2) {
+                for (let j = 0; j < 6; j += 2) {
+                    relativeSpots.push([i, j], [i * -1, j], [i * -1, j * -1], [i, j * -1])
+                }
+            }
 
-      let toBuild = Math.floor(CONTROLLER_STRUCTURES['extension'][room.controller.level] / 4) * 4;
-      relativeSpots.slice(0, toBuild).forEach(function(relCoords) {
-        ensureBuilding(room, spawn.pos.getRelativePosition(relCoords[0], relCoords[1]), STRUCTURE_EXTENSION);
-      })
-    });
-  }
+            let toBuild = Math.floor(CONTROLLER_STRUCTURES['extension'][room.controller.level] / 4) * 4;
+            relativeSpots.slice(0, toBuild).forEach(function(relCoords) {
+                ensureBuilding(room, spawn.pos.getRelativePosition(relCoords[0], relCoords[1]), STRUCTURE_EXTENSION);
+            })
+        });
+    }
 };
 
 let constructTowers = function(room) {
-  // Construct defense towers
-  let locations = [
-    [25, 25],
-    // TODO(kahlers): Add more locations for towers
-  ];
+    // Construct defense towers
+    let locations = [
+        [25, 25], // Center
+        [13, 25], // X axis
+        [38, 25],
+        [25, 13], // Y axis
+        [25, 38],
+    ];
 
-  locations.forEach(function(loc) {
-    // TODO(kahlers): Add logic to find a suitable spot if location is blocked
-    ensureBuilding(room, new RoomPosition(loc[0], loc[1], room.name), STRUCTURE_TOWER);
-  });
+    locations.forEach(function(loc) {
+        // TODO(kahlers): Add logic to find a suitable spot if location is blocked
+        ensureBuilding(room, new RoomPosition(loc[0], loc[1], room.name), STRUCTURE_TOWER);
+    });
 };
 
 module.exports = function(room) {
 
-  constructExtensions(room);
-  constructTowers(room);
+    constructExtensions(room);
+    constructTowers(room);
 
 };
